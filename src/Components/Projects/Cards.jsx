f85e5f5fc79ab888df74876f1c811678ec1008f4
lbenjaminq <benjaminlbq12@gmail.com>
2022-09-28 11:55:06 -0300

    Feat(src): New Projects

--- src/Components/Projects/Cards.jsx
+++ src/Components/Projects/Cards.jsx
@@ -1,57 +1,33 @@
 import style from "../../Styles/Projects.module.css";
-import CountriesApp from "../../Images/CountriesApp.png";
-import B2B from "../../Images/B2BCommodities.png";
 import githubRepo from "../../Images/githubRepo.png";
 import web from "../../Images/web.png";
+import { projects } from "../../Projects";
 
 function GroupExample() {
+
   return (
     <section id="Proyectos" className={style.container_cards}>
-      <div className={style.card_one}>
-        <a href="https://pi-countries-sigma.vercel.app/" target={"blank"}>
-          <img
-            src={CountriesApp}
-            className={style.projects}
-            alt="CountriesApp"
-          />
-        </a>
-        <h3>Countries app</h3>
-        <p>
-          Single Page Aplication (SPA) para usuarios donde pueden buscar y/o
-          crear actividades en cada país del mundo, filtrar y ordernar los
-          mismos por distintos parámetros.
-        </p>
-        <div className={style.social_media}>
-          <a href="https://github.com/lbenjaminq/PI-Countries" target={"blank"}>
-            <img src={githubRepo} alt="github" />
-          </a>
+      {projects.map((project) => (
+        <div className={style.card_one} key={project.name}>
           <a href="https://pi-countries-sigma.vercel.app/" target={"blank"}>
-            <img src={web} style={{ width: "25px" }} alt="CountriesApp" />
+            <img
+              src={project.img}
+              className={style.projects}
+              alt={project.name}
+            />
           </a>
+          <h3>{project.name}</h3>
+          <p>{project.overview}</p>
+          <div className={style.social_media}>
+            <a href={project.link} target={"blank"}>
+              <img src={githubRepo} alt="github" />
+            </a>
+            <a href={project.deploy} target={"blank"}>
+              <img src={web} style={{ width: "25px" }} alt={project.name} />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className={style.card_two}>
-        <a href="https://dev-commodities.vercel.app/" target={"blank"}>
-          <img src={B2B} className={style.projects} alt="B2B" />
-        </a>
-        <h3>B2B Commodities</h3>
-        <p>
-          Foro Online B2B (Bussines to Bussines) para importadores/exportadores
-          de Commodities a nivel internacional. Los usuarios generan “posteos”
-          donde puedan ofrecer/buscar productos.
-        </p>
-        <div className={style.social_media}>
-          <a
-            href="https://github.com/BarberoPablo/Commodities-Project"
-            target={"blank"}
-          >
-            <img src={githubRepo} alt="github" />
-          </a>
-          <a href="https://dev-commodities.vercel.app/" target={"blank"}>
-            <img src={web} style={{ width: "25px" }} alt="B2B" />
-          </a>
-        </div>
-      </div>
+      ))}
     </section>
   );
 }
---
+++ src/Projects.js
@@ -0,0 +1,35 @@
+import CountriesApp from "./Images/CountriesApp.png"
+import B2B from "./Images/B2BCommodities.png"
+import NetflixClone from "./Images/NetflixClone.png"
+import TeslaClone from "./Images/TeslaClone.png"
+
+export const projects = [
+  {
+    name:"Countries app",
+    overview:"Single Page Aplication (SPA) para usuarios donde pueden buscar y/o crear actividades en cada país del mundo, filtrar y ordernar los mismos por distintos parámetros.",
+    link:"https://github.com/lbenjaminq/PI-Countries",
+    deploy:"https://pi-countries-sigma.vercel.app/",
+    img:CountriesApp
+  },
+  {
+    name:"B2B Commodities",
+    overview:" Foro Online B2B (Bussines to Bussines) para importadores/exportadores de Commodities a nivel internacional. Los usuarios generan “posteos” donde puedan ofrecer/buscar productos.",
+    link:"https://github.com/BarberoPablo/Commodities-Project",
+    deploy:"https://dev-commodities.vercel.app/",
+    img:B2B
+  },
+  {
+    name:"Netflix Clone",
+    overview:"Clon de Netflix hecho en React, implementando Firebase para el manejo de usuarios y Material UI para los estilos",
+    link:"https://github.com/lbenjaminq/NetflixClone",
+    deploy:"https://netflux-bice.vercel.app/",
+    img:NetflixClone
+  },
+  {
+    name:"Tesla Clone",
+    overview:"Clon de Tesla hecho en React, hecho al 100% con Styled Components",
+    link:"https://github.com/lbenjaminq/teslaClone",
+    deploy:"https://tesla-clone-tau-snowy.vercel.app/",
+    img:TeslaClone
+  }
+]
